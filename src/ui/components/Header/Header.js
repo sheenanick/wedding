9538b30f4354afbeaff6c997125e025896d6cb5d
sheenanick <sheenanick@gmail.com>
2017-09-11 16:01:21 -0700

    Change header copy

--- src/ui/components/Header/Header.js
+++ src/ui/components/Header/Header.js
@@ -10,13 +10,13 @@ class Header extends Component {
           <div className='header-content'>
             <div className='title-container'>
               <div className='header-title cursive'>
-                Sheena
+                #Winter
               </div>
               <div className='header-title cursive'>
-                &
+                WonDo
               </div>
               <div className='header-title cursive'>
-                Trong
+                land
               </div>
             </div>
             <Countdown date={this.props.date} loading={this.props.loading}/>
