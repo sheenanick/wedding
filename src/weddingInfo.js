33dcebfceaff77d1c1d69a0f6ed6d841c59e3c91
sheenanick <sheenanick@gmail.com>
2017-10-17 21:45:50 -0700

    Update ending time

--- src/weddingInfo.js
+++ src/weddingInfo.js
@@ -4,23 +4,12 @@ export const WEDDING_INFO = {
     "title": "Ceremony & Reception",
     "date" : "2018-02-17 16:00:00",
     "startTime": "4:00 PM",
-    "endTime": "10:00 PM",
+    "endTime": "9:00 PM",
     "address": {
       "name": "NITA LAKE LODGE",
       "street": "2131 Lake Placid Road",
       "cityzip": "Whistler, B.C. V0N 1B2",
       "map": "https://www.google.com/maps/place/Nita+Lake+Lodge/@50.0964651,-122.9979783,17z/data=!3m1!4b1!4m5!3m4!1s0x548722d182d467f3:0x914930a812f280ee!8m2!3d50.0964651!4d-122.995795",
     },
-  },
-  "reception" : {
-    "title": "Reception",
-    "startTime": "5:00 PM",
-    "endTime": "8:00 PM",
-    "address": {
-      "name": "Cathedral Park",
-      "street": "123 SE Main St",
-      "cityzip": "Portland, OR 97205",
-      "map": "www.google.com",
-    }
   },
 }
