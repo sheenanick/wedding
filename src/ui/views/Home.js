0d75b7782841d72626232b4d62de2b9225aeaa7d
sheenanick <sheenanick@gmail.com>
2017-12-07 11:49:16 -0800

    Move snow to Home route only; remove spaces from stylesheets

--- src/ui/views/Home.js
+++ src/ui/views/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux'
 import { browserHistory } from 'react-router';
 import Header from '../components/Header/Header';
 import Details from '../components/Details/Details';
@@ -11,10 +10,34 @@ class Home extends Component {
     browserHistory.push('/');
   }
 
+  renderSnow() {
+    const positions = [];
+    // determine size of positions array based on window width
+    const values = window.innerWidth / 15;
+    // add random position values from 1 to 97 %
+    for (var i = 0; i < values; i++) {
+      positions[i] = Math.floor(Math.random() * 97 + 1);
+    }
+    return (
+      //render a snowflake at every position with random animation and image
+      positions.map((position, index) => {
+        const style = {
+          left: `${position}%`,
+          animationDuration: `${Math.floor(Math.random() * 35 + 20)}s`,
+          animationDelay: `${Math.floor(Math.random() * 30)}s`
+        }
+        return(
+          <img className='snowflake' src={require(`../../img/icons/snowflake${Math.floor(Math.random() * 4 + 1)}.png`)} alt='snowflake' style={style} key={index} />
+        );
+      })
+    );
+  }
+
   render() {
     return (
       <div>
-        <Header date={this.props.date} loading={this.props.loading} details={WEDDING_INFO.ceremony}/>
+        {this.renderSnow()}
+        <Header details={WEDDING_INFO.ceremony}/>
         <div className='home-content'>
           <Details id='ceremony'
             details={WEDDING_INFO.ceremony}/>
@@ -24,11 +47,4 @@ class Home extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    date: state.countdownState.date,
-    loading: state.countdownState.loading,
-  }
-}
-
-export default connect(mapStateToProps)(Home);
+export default Home;
