d7a907c051b7b3f96103212c8cea8bc12da1ff91
sheenanick <sheenanick@gmail.com>
2018-02-08 21:27:43 -0800

    Update wedding date

--- src/weddingInfo.js
+++ src/weddingInfo.js
@@ -1,8 +1,8 @@
 export const WEDDING_INFO = {
-  "date" : "2018-01-27 16:00:00",
+  "date" : "2018-12-31 16:00:00",
   "ceremony" : {
     "title": "Ceremony & Reception",
-    "date" : "2018-01-27 16:00:00",
+    "date" : "2018-12-31 16:00:00",
     "startTime": "4:00 PM",
     "endTime": "10:00 PM",
     "address": {
@@ -11,16 +11,5 @@ export const WEDDING_INFO = {
       "cityzip": "Whistler, B.C. V0N 1B2",
       "map": "https://www.google.com/maps/place/Nita+Lake+Lodge/@50.0964651,-122.9979783,17z/data=!3m1!4b1!4m5!3m4!1s0x548722d182d467f3:0x914930a812f280ee!8m2!3d50.0964651!4d-122.995795",
     },
-  },
-  "reception" : {
-    "title": "Reception",
-    "startTime": "5:00 PM",
-    "endTime": "8:00 PM",
-    "address": {
-      "name": "Cathedral Park",
-      "street": "123 SE Main St",
-      "cityzip": "Portland, OR 97205",
-      "map": "www.google.com",
-    }
   },
 }
